3f3ea4cc8df42e280f5fa9df75fc35b93eaacf14
SAKOUHIGHADA <Ghada.SAKOUHI@esprit.tn>
2026-06-17 20:07:38 +0100

    update finale

--- src/main/java/tn/esprit/studentmanagement/controllers/DepartmentController.java
+++ src/main/java/tn/esprit/studentmanagement/controllers/DepartmentController.java
@@ -6,6 +6,7 @@ import tn.esprit.studentmanagement.services.DepartmentService;
 import tn.esprit.studentmanagement.services.IDepartmentService;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.Data;
 
 import java.util.List;
 
